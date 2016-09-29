426ea079dc4c8db8d562e624cafca39de653a8e9
Akihiro Imigi <imigi@uni-info.co.jp>
2016-09-29 13:53:07 +0900

    プロジェクト ファイルを追加します。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace App1
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri newUri;
+            if (Uri.TryCreate(this.textBox1.Text, UriKind.Absolute, out newUri)
+            && newUri.Scheme.StartsWith("http"))
+            {
+                webView1.Navigate(newUri);
+            }
+            else
+            {
+                // エラー処理
+                string errMsg = @"入力された文字列がURIとして不正か、""http""で始まっていません";
+                await new MessageDialog(errMsg).ShowAsync();
+            }
+        }
+    }
+}
